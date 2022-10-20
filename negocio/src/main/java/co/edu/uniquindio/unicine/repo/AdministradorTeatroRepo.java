3ce7dd8d6a431ecaf5121604140c0bd1b01afb0e
Phillips <juanf.lpezc@uqvirtual.edu.co>
2022-10-19 23:59:42 -0500

    Servicios del administrador de teatro y pocos querys

--- negocio/src/main/java/co/edu/uniquindio/unicine/repo/AdministradorTeatroRepo.java
+++ negocio/src/main/java/co/edu/uniquindio/unicine/repo/AdministradorTeatroRepo.java
@@ -1,7 +1,9 @@
 package co.edu.uniquindio.unicine.repo;
 
 import co.edu.uniquindio.unicine.entidades.AdministradorTeatro;
+import co.edu.uniquindio.unicine.entidades.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdministradorTeatroRepo extends JpaRepository<AdministradorTeatro, String> {
+    AdministradorTeatro findByEmailAndContrasena(String email, String contrasena);
 }
