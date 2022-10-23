16d31142bf9ab7bbcc2cb3121e305ac2b20e4dbb
Samuel Echeverri <eb.samuel6@gmail.com>
2022-10-22 20:18:44 -0500

    Tests para ClienteServicio terminados, correcciones menores en varios otros archivos

--- negocio/src/main/java/co/edu/uniquindio/unicine/entidades/CompraConfiteria.java
+++ negocio/src/main/java/co/edu/uniquindio/unicine/entidades/CompraConfiteria.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
 @RequiredArgsConstructor
 @Getter
 @Setter
-@ToString()
+@ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class CompraConfiteria implements Serializable {
     // Atributos -----------------------------------------------------------------------------------------
