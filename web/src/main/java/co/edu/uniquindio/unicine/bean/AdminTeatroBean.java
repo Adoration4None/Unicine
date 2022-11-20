8ad0d3889fb569540039eb3f1a0d93ac4212e101
Phillips <juanf.lpezc@uqvirtual.edu.co>
2022-11-20 16:40:02 -0500

    Info del administrador meleada

--- web/src/main/java/co/edu/uniquindio/unicine/bean/AdminTeatroBean.java
+++ web/src/main/java/co/edu/uniquindio/unicine/bean/AdminTeatroBean.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.primefaces.PrimeFaces;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -24,6 +25,11 @@ public class AdminTeatroBean implements Serializable {
 
     @Autowired
     private AdministradorServicio administradorServicio;
+
+    @Getter
+    @Setter
+    @Value(value="#{seguridadBean.personaIngresada}")
+    private AdministradorTeatro administradorTeatro;
 
     @Getter
     @Setter
